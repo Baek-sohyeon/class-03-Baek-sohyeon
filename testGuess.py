fb54414c4a381c2be3336d0e94e7f9783fb454c0
Baek-sohyeon <55051191+Baek-sohyeon@users.noreply.github.com>
2019-11-28 10:51:27 +0900

    Update testGuess.py

--- testGuess.py
+++ testGuess.py
@@ -43,5 +43,32 @@ class TestGuess(unittest.TestCase):
         self.g1.guess('s')
         self.assertEqual(self.g1.displayGuessed(), ' a e n t u d k s')
 
+    def testReturnFinished(self):
+        self.g1.guess('d')
+        self.assertEqual(self.g1.finished(), False)
+        self.g1.guess('e')
+        self.assertEqual(self.g1.finished(), False)
+        self.g1.guess('f')
+        self.assertEqual(self.g1.finished(), False)
+        self.g1.guess('a')
+        self.assertEqual(self.g1.finished(), False)
+        self.g1.guess('u')
+        self.assertEqual(self.g1.finished(), False)
+        self.g1.guess('l')
+        self.assertEqual(self.g1.finished(), False)
+        self.g1.guess('t')
+        self.assertEqual(self.g1.finished(), True)
+
+    def testReturnGuessed(self):
+        self.assertEqual(self.g1.guess('e'), True)
+        self.assertEqual(self.g1.guess('f'), True)
+        self.assertEqual(self.g1.guess('a'), True)
+        self.assertEqual(self.g1.guess('A'), False)
+        self.assertEqual(self.g1.guess('t'), True)
+        self.assertEqual(self.g1.guess('l'), True)
+        self.assertEqual(self.g1.guess('u'), True)
+        self.assertEqual(self.g1.guess('d'), True)
+        self.assertEqual(self.g1.guess('k'), False)
+
 if __name__ == '__main__':
     unittest.main()
